65646f4f3321affc3d2973dbd1e1512621daba5a
Balazs Kereszturi <19balazs86@gmail.com>
2020-05-16 11:47:22 +0200

    Add empty console client

---
+++ Grpc.Demo.ConsoleClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Grpc.Demo.ConsoleClient
+{
+  public static class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
