0e7c753e797bd6b72b2fe83479a96acc73e3cf14
Balazs Kereszturi <19balazs86@gmail.com>
2020-05-16 14:25:50 +0200

    Add ConsoleClient

--- Grpc.Demo.ConsoleClient/Program.cs
+++ Grpc.Demo.ConsoleClient/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using GreeterService;
+using Grpc.Net.Client;
 
 namespace Grpc.Demo.ConsoleClient
 {
@@ -7,7 +10,26 @@ namespace Grpc.Demo.ConsoleClient
   {
     public static async Task Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      var client = createClient();
+
+      HelloRequest request = new HelloRequest { Name = "John Doe" };
+
+      // Call: SayHello
+      HelloReply reply = await client.SayHelloAsync(request);
+
+      Console.WriteLine(reply.Message);
+
+      // Call: SayHelloToNobody
+      reply = await client.SayHelloToNobodyAsync(new Empty());
+
+      Console.WriteLine(reply.Message);
+    }
+
+    private static Greeter.GreeterClient createClient()
+    {
+      GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+      return new Greeter.GreeterClient(channel);
     }
   }
 }
